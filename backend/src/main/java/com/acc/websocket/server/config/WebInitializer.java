bcee08ece772225b5b9b7727c88bfa4db650ac71
Cristi Ando Ciupav <accexpert@gmail.com>
2016-12-10 23:07:43 +0200

    add partial ws clients frontend files

--- backend/src/main/java/com/acc/websocket/server/config/WebInitializer.java
+++ backend/src/main/java/com/acc/websocket/server/config/WebInitializer.java
@@ -1,5 +1,12 @@
 package com.acc.websocket.server.config;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
+@Component
+@EnableWebSocket
+@EnableWebMvc
 public class WebInitializer {
+
 }
